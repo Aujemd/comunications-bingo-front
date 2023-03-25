a249c3ec90aa1416820732c63ad1885cb84b2783
Johan <joenmodu99@gmail.com>
2023-03-25 14:20:27 -0400

    feat: initial front added

--- src/App.js
+++ src/App.js
@@ -1,25 +1,94 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect } from 'react'
+
+import Box from '@mui/material/Box'
+import InputLabel from '@mui/material/InputLabel'
+import MenuItem from '@mui/material/MenuItem'
+import FormControl from '@mui/material/FormControl'
+import Select from '@mui/material/Select'
+import TextField from '@mui/material/TextField'
+import Button from '@mui/material/Button'
+import Typography from '@mui/material/Typography'
+import { socket } from './socket'
 
 function App() {
+  const [mode, setMode] = React.useState('')
+  const [playerName, setPlayerName] = React.useState('')
+  const [modeInput, setModeInput] = React.useState(false)
+
+  const handleChangeName = (event) => {
+    setPlayerName(event.target.value)
+  }
+  const handleChangeMode = (event) => {
+    setMode(event.target.value)
+
+    socket.emit('set-mode', event.target.value)
+  }
+
+  const connect = () => {
+    socket.emit('request-game', {
+      playerName
+    })
+  }
+
+  useEffect(() => {
+    socket.connect()
+
+    socket.on('joined-game', (e) => {
+      console.log(e)
+    })
+
+    socket.on('player-disconnected', (e) => {
+      console.log('Se desconecto', e)
+    })
+
+    socket.on('player-connected', (e) => {
+      console.log('Se conecto', e)
+    })
+
+    socket.on('table-assigned', (e) => {
+      console.log('Carton asignado', e)
+    })
+    socket.once('lobby-closed', (e) => {
+      setModeInput(true)
+    })
+
+    return () => {
+      socket.off('joined-game')
+      socket.off('player-disconnected')
+    }
+  }, [])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      <Typography variant='h1' component='h1'>
+        Bingo
+      </Typography>
+      <Box sx={{ minWidth: 120, marginTop: 10, component: 'form' }}>
+        <FormControl fullWidth sx={{ marginBottom: 5 }}>
+          <TextField
+            label='Ingresa tu nombre jugador'
+            variant='outlined'
+            onChange={handleChangeName}
+          />
+        </FormControl>
+        <FormControl fullWidth>
+          <InputLabel>Selecciona el modo de juego</InputLabel>
+          <Select
+            value={mode}
+            label='Selecciona el modo de juego'
+            onChange={handleChangeMode}
+            disabled={modeInput}
+          >
+            <MenuItem value={'NORMAL'}>NORMAL</MenuItem>
+            <MenuItem value={'FULL'}>FULL</MenuItem>
+          </Select>
+        </FormControl>
+        <Button variant='outlined' sx={{ marginTop: 5 }} onClick={connect}>
+          Conectarse
+        </Button>
+      </Box>
+    </>
+  )
 }
 
-export default App;
+export default App
---
+++ src/socket.js
@@ -0,0 +1,5 @@
+import { io } from 'socket.io-client'
+
+export const socket = io(process.env.SOCKET_URL || 'http://localhost:3000', {
+  autoConnect: false
+})
